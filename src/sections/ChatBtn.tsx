951fb261af46d14be1539dec3f71746959459b64
Azhar Khan <107482530+Alkhanazhar@users.noreply.github.com>
2025-01-09 03:38:00 +0530

    first commit

--- src/sections/ChatBtn.tsx
+++ src/sections/ChatBtn.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
-import { FaCross } from "react-icons/fa";
 import { MdCancel, MdMessage, MdSend } from "react-icons/md";
 
 const ChatBtn = () => {
--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,4 +1,3 @@
-import { FC } from "react";
 
 const Intro = () => {
   return <div>Intro</div>;
