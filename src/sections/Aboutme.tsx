e33fd487793125fa9d4d899b5a2fcba1aa3d0944
Azhar Khan <107482530+Alkhanazhar@users.noreply.github.com>
2025-01-10 01:30:46 +0530

    about me

--- src/sections/Aboutme.tsx
+++ src/sections/Aboutme.tsx
@@ -2,11 +2,26 @@
 import { motion } from "framer-motion";
 import React from "react";
 import Title from "./Title";
+import { TextGenerateEffect } from "@/components/components/ui/text-generate-effect";
+
+// stohp stohp stohp stohp stohp stohp stohp
 
 const AboutMe = () => {
+  const words = ` I'm Azhar Khan, I'm a front-end full stack from
+          India. I've been learning programming for over 3 years. After I
+          started high school, I decided to pursue my passion for programming.
+          My favorite part of programming is the problem-solving aspect. I love
+          the feeling of finally figuring out a solution to a problem. I've
+          had the opportunity to work on several exciting projects during my
+          learning journey, ranging from personal projects to collaborative
+          ventures. My core stack is React, Next.js, TypeScript, and Tailwind
+          CSS. I am also familiar with Postgresql and Prisma. I am always
+          looking to learn new technologies. /n  I'm open to Job opportunities where I can contribute, learn and
+          grow. If you have a good opportunity that matches my skills and
+          experience then don't hesitate to contact me.`;
   return (
     <motion.section
-      className="my-12 flex w-full scroll-mt-28 flex-col items-center py-12"
+      className="my-12 flex w-full scroll-mt-28 flex-col items-center py-12 relative"
       initial={{ opacity: 0, scale: 0.9 }}
       whileInView={{ opacity: 1, scale: 1 }}
       transition={{ duration: 0.2 }}
@@ -14,23 +29,8 @@ const AboutMe = () => {
     >
       <Title title="About Me" />
       <div className="max-w-2xl text-center leading-7 py-10 ">
-        <p className="mb-4 text-white/70">
-          I&apos;m Azhar Khan, I&apos;m a front-end full stack developer from
-          India. I&apos;ve been learning programming for over 3 years. After I
-          started high school, I decided to pursue my passion for programming.
-          My favorite part of programming is the problem-solving aspect. I love
-          the feeling of finally figuring out a solution to a problem. I&apos;ve
-          had the opportunity to work on several exciting projects during my
-          learning journey, ranging from personal projects to collaborative
-          ventures. My core stack is React, Next.js, TypeScript, and Tailwind
-          CSS. I am also familiar with Postgresql and Prisma. I am always
-          looking to learn new technologies.
-        </p>
-        <p>
-          I&apos;m open to Job opportunities where I can contribute, learn and
-          grow. If you have a good opportunity that matches my skills and
-          experience then don&apos;t hesitate to contact me.
-        </p>
+        <div className="w-60 h-60 bg-blue-500 blur-3xl absolute top-1/2 left-1/2"></div>
+        <TextGenerateEffect className={"text-white/50"} words={words} />
       </div>
     </motion.section>
   );
--- src/sections/ChatBtn.tsx
+++ src/sections/ChatBtn.tsx
@@ -13,72 +13,76 @@ const ChatBtn = () => {
     // Open the WhatsApp link in a new tab
   };
   return (
-    <>
+    <div className="fixed bottom-12 right-12 ">
       <AnimatePresence>
-        {chatbox && (
-          <motion.div
-            initial={{ scale: 0.5, opacity: 0, y: 10, skew: 0.4 }}
-            animate={{ opacity: 1, y: 0, scale: 1 }}
-            exit={{ opacity: 0, y: 10, scale: 0.5 }}
-            transition={{ duration: 0.2 }}
-            className="flex flex-col justify-end p-4 origin-bottom-right gap-4 border h-52 fixed bottom-12 right-12  z-50 bg-blue-600/30 backdrop-blur-2xl rounded-2xl"
-          >
-            <MdCancel
-              onClick={() => setBox((prev) => !prev)}
-              className="absolute top-3 right-3 cursor-pointer"
-            />
-            <h3>
-              Send message to us <br /> We will connect soon
-            </h3>
-            <div className="flex mt-auto gap-4">
-              <input
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                className="text-black p-2"
-                placeholder="CONNECT WITH US"
-              />
-              <a
-                href={`https://wa.me/+9109093125?=${encodeURIComponent(
-                  message
-                )}!`}
-                target="_blank"
-                className="px-4 bg-blue-500 text-white inline-block py-3"
-                onClick={handleClick}
-              >
-                <MdSend />
-              </a>
-            </div>
-          </motion.div>
-        )}
-      </AnimatePresence>
-      <motion.div
-        whileHover={{ scale: 1.2 }}
-        className="fixed bottom-6 right-6 rounded-full p-4 bg-gradient-to-r from-blue-700 to-blue-800 cursor-pointer"
-        onClick={() => setBox((prev) => !prev)}
-      >
-        <AnimatePresence>
-          {!chatbox ? (
-            <motion.div
-              initial={{ scale: 0, opacity: 0, y: 10, skew: 0.4 }}
-              animate={{ opacity: 1, y: 0, scale: 1.5 }}
-              exit={{ opacity: 0, y: 10, scale: 0 }}
-              transition={{ duration: 0.2 }}
-            >
-              <MdMessage />{" "}
-            </motion.div>
-          ) : (
+        <motion.div layout>
+          {chatbox && (
             <motion.div
               initial={{ scale: 0.5, opacity: 0, y: 10, skew: 0.4 }}
-              animate={{ opacity: 1, y: 0, scale: 1.5 }}
+              animate={{ opacity: 1, y: 0, scale: 1 }}
               exit={{ opacity: 0, y: 10, scale: 0.5 }}
               transition={{ duration: 0.2 }}
+              className="min-h-52 flex flex-col justify-end p-4 origin-bottom-right gap-4 border  z-50 bg-blue-600/30 backdrop-blur-2xl rounded-2xl"
             >
-              <MdCancel />
+              <MdCancel
+                onClick={() => setBox((prev) => !prev)}
+                className="absolute top-3 right-3 cursor-pointer"
+              />
+              <h3>
+                Send message to us <br /> We will connect soon
+              </h3>
+              <div className="flex mt-auto gap-4">
+                <input
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  className="text-black p-2 rounded-md shadow-lg"
+                  placeholder="CONNECT WITH US"
+                />
+                <a
+                  href={`https://wa.me/+9109093125?=${encodeURIComponent(
+                    message
+                  )}!`}
+                  target="_blank"
+                  className="px-4 bg-blue-500 text-white inline-block py-3 rounded-md shadow-md"
+                  onClick={handleClick}
+                >
+                  <MdSend />
+                </a>
+              </div>
             </motion.div>
           )}
-        </AnimatePresence>
-      </motion.div>
-    </>
+          <motion.div
+            layout
+            whileHover={{ scale: 1.2 }}
+            className=" rounded-full p-4 bg-gradient-to-r cursor-pointer"
+            onClick={() => setBox((prev) => !prev)}
+          >
+            {!chatbox ? (
+              <motion.div
+                initial={{ scale: 0, opacity: 0, y: 10, skew: 0.4 }}
+                animate={{ opacity: 1, y: 0, scale: 1.5 }}
+                exit={{ opacity: 0, y: 10, scale: 0 }}
+                transition={{ duration: 0.2 }}
+              >
+                <motion.span layout className="flex justify-center">
+                  <MdMessage />{" "}
+                </motion.span>
+              </motion.div>
+            ) : (
+              <motion.div
+                initial={{ scale: 0.5, opacity: 0, y: 10, skew: 0.4 }}
+                animate={{ opacity: 1, y: 0, scale: 1.5 }}
+                exit={{ opacity: 0, y: 10, scale: 0.5 }}
+                transition={{ duration: 0.2 }}
+                className="blur-3xl bg-red-200/20"
+              >
+                <MdCancel />
+              </motion.div>
+            )}
+          </motion.div>
+        </motion.div>
+      </AnimatePresence>
+    </div>
   );
 };
 
