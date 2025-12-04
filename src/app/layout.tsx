3bfdc0a25f81d33fd7c8c249e95c54f1f5ded7fd
alkhanazhar <khan.azhar.saf@gmail.com>
2025-12-04 17:21:48 +0530

    new changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Minimal Single Page Portfolio",
   description: "Created with Frontend Tribe",
 };
+
+const inter = Inter({
+  variable: "--font-inter",
+  weight: ["100", "200", "300", "500", "700", "800"],
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={"antialiased bg-black text-white"}>{children}</body>
+      <body className={`antialiased bg-black text-white ${inter.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
